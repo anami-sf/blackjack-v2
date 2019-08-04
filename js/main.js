6b0669e63890b460e13f73a8a943259aff3a71f9
anami823 <anami823@gmail.com>
2019-08-04 13:47:02 -0700

    Calculate each hand's score

--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@
 //var dealerCards = $('#dealer-hand').children().length
 //var playerCards = $('#player-hand').children().length
 
-let dealtCard;
-
 const numArr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A' ];
 const suitArr = ['H', 'D', 'S', 'C' ];
 
 let cardDeck = [];
 
 const buildCardDeck = (arr1, arr2) => { 
-    
+ 
+    //TODO: Move value function outside buildCardDeck
+
     for(const num of arr1){
         for(const suit of arr2){
             const value= (num) => {
@@ -40,7 +40,8 @@ const buildCardDeck = (arr1, arr2) => {
 
 
 /*----- app's state (variables) -----*/ 
-let playerHand, dealerHand, turn;
+let playerHand, playerScore, dealerHand, dealerScore, turn, dealtCard, stay, winner, bust;
+
 /*----- cached element references -----*/
 
 
@@ -50,13 +51,17 @@ let playerHand, dealerHand, turn;
 const Initialize = () => {
     buildCardDeck(numArr, suitArr)
     playerHand = []
+    playerScore = 0
     dealerHand = []
+    dealerScore = 0
     turn = true
+    stay = false
+    bust = false
 }
 
 Initialize()
 
-const iterate = (handEl, hand) => {
+const renderCardImg = (handEl, hand) => {
     for (card of hand) {
         $(`#${handEl}`).append(`<img class="cardImg" src=${card.img}  alt="card">`)
     }
@@ -67,36 +72,86 @@ const render = () => {
     $(`#dealer-hand`).html("")
     $(`#player-hand`).html("")
     
-    iterate('player-hand', playerHand)
-    iterate('dealer-hand', dealerHand)
+    renderCardImg('player-hand', playerHand)
+    renderCardImg('dealer-hand', dealerHand)
 }
 
+// TODO: turn into arrow function
+function isBust() {
+    if (playerScore > 21){
+        bust = true
+        return true
+    }    
+}
 
-const draw = (hand) => {
-    const dealtCard = cardDeck.pop()
+const getScore = (hand) => {
+
+    var handScore = hand.reduce( (total, card) => {
+        return total + card.value
+    },0)
+
+    return handScore
+}
+
+// if (hand === playerHand){
+//     playerHand = handScore
+// } else if (hand === dealerHand) {
+//     dealerHand = handScore
+// }  
+
+const draw = () => {
+    dealtCard = cardDeck.pop()
     return dealtCard
 }
 
 const deal = (evt) => {
 
-    const dealtCard1 = cardDeck.pop()
-    const dealtCard2 = cardDeck.pop()
-    
     if (evt.target.id === "hit") {
-        playerHand.push(dealtCard)
+        playerHand.push(draw())
+        playerScore = getScore(playerHand)
     } else if (evt.target.id === "stay") {
-        dealerHand.push(dealtCard)
+        stay = true
+        while( dealerScore < 18) {
+            dealerHand.push(draw()) 
+            dealerScore = getScore(dealerHand)        
+        }             
     } else {
 
         playerHand = []
         dealerHand = []
 
         while(playerHand.length < 2) {
-            playerHand.push(dealtCard1)
-            dealerHand.push(dealtCard2)
+            playerHand.push(draw())
+            dealerHand.push(draw())
+            playerScore = getScore(playerHand)
+            dealerScore = getScore(dealerHand) 
         }
     }
+    getStatus()
     render()
+}
+
+const getStatus = () => {
+    
+    isBust()
+
+    if (bust) {
+        console.log('Dealer wins')
+    } else if (!stay) {
+        console.log('Player Turn')
+    } else if (stay) {
+        checkForWinner()
+        console.log('winner: ', winner)
+    }
+    console.log('playerScore: ', playerScore, ' dealerScore: ', dealerScore)
+}
+
+const checkForWinner = () => {
+    if ( playerScore === 21 || (playerScore > dealerScore)){
+        winner = 'player'
+    } else {
+        winner = 'dealer'
+    }
 }
 
 /*----- event listeners -----*/ 
