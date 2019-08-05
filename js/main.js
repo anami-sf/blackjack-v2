dccb076e5e3cdc73b919a8c61a4dc55095a0a7b9
anami823 <anami823@gmail.com>
2019-08-05 15:40:01 -0700

    Scramble Deck

--- js/main.js
+++ js/main.js
@@ -38,6 +38,7 @@ const buildCardDeck = (arr1, arr2) => {
             cardDeck.push(cardObj);
         }
     }
+    return cardDeck
 }
 
 
@@ -50,8 +51,28 @@ let playerHand, playerScore, dealerHand, dealerScore, dealtCard, stay, winner, g
 
 /*----- functions -----*/
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+
 const Initialize = () => {
-    buildCardDeck(numArr, suitArr)
+    shuffle(buildCardDeck(numArr, suitArr))
     playerHand = []
     playerScore = 0
     dealerHand = []
